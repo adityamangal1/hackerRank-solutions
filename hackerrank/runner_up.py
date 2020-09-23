7d8b58f521ac23f9765e02f273d9cb48f4f4b979
aditya mangal <adityamangal0202@gmail.com>
2020-09-23 16:05:29 +0530

    initial commit

---
+++ hackerrank/runner_up.py
@@ -0,0 +1,11 @@
+
+# # find the secound runner up
+# user2 = list(map(int, input().split()))
+# print(user2)
+# list = [i for i in user2 if i != max(user2)]
+# print(max(list))
+
+user = list(map(int, input().split()))
+print(user)
+
+
