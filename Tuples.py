489e8428ab563843fd6beca1bd987c78b0fc0985
aditya mangal <adityamangal0202@gmail.com>
2020-09-25 12:42:02 +0530

    initial commit

---
+++ Tuples.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = list(map(int, input().split()))
+
+    print(integer_list)
+    a = tuple(integer_list)
+    print(a)
+    print(hash(a))
