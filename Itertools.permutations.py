d53ca7d4c48b67b69818c86d141041ab46733e9b
aditya mangal <adityamangal0202@gmail.com>
2020-10-03 16:49:58 +0530

    initial commit

---
+++ Itertools.permutations.py
@@ -0,0 +1,14 @@
+from itertools import permutations
+
+x = word, num = input().split()
+a = list(permutations(word, int(num)))
+
+b = list(map(list, a))
+
+final_list = []
+for i in range(len(b)):
+    final_list.append(''.join(b[i]))
+
+a = sorted(final_list)  # sorted function
+for i in range(len(a)):
+    print(a[i])
