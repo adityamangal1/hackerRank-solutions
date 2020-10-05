4c548b0f265499f9100fe526cd9faa5b96828be9
aditya mangal <adityamangal0202@gmail.com>
2020-10-05 16:33:37 +0530

    new solution

---
+++ Merge the Tools!.py
@@ -0,0 +1,20 @@
+'''
+Author: Aditya Mangal 
+Date:  5 october,2020
+Purpose: python practise problem
+
+'''
+user = input()
+user2 = int(input())
+
+subsequent = int(len(user)//user2)
+for i in range(subsequent):
+
+    slicing = user[i * user2:(i + 1) * user2]
+
+    u = ""
+    for c in slicing:
+        if c not in u:
+            u += c
+
+    print(u)
